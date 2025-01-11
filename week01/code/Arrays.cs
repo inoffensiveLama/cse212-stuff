466fd1081015d6180b17d6a58023a07cb1006c3d
janZ <janzander96@gmail.com>
2025-01-11 14:39:13 +0800

    finished the array problems for week 1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,20 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // first of all I will create a new array with the length that matches the number of multiples
+        // then I will make a for loop that puts the multiple of the number in the index of that iteration
+        // lastly I will return the array
+
+        //create array with given length
+        double[] result = new double[length];
+
+        //multiply the number and add to the result array
+        for (int i = 0; i < length; i++){
+            result[i] = (i + 1) * number;
+        }
+
+        //return the result array
+        return result; 
     }
 
     /// <summary>
@@ -29,5 +42,23 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // I love modulo, its like magic. Best thing programming has to offer. So I will solve this issue using it.
+        // so first of all I will need to create a new List.
+        // then I will iterate through the original list and find the new list with this algorithm: (originalIndex + rotationAmount) % lengthOfList
+
+        //create new array to store the results in
+        int[] result = new int[data.Count];
+
+        // iterate over the list and assign the new index numbers using the algorithm: (originalIndex + rotationAmount) % lengthOfList
+        for (int i = 0; i < data.Count; i++) {
+            int index = (i + amount) % data.Count;
+            result [index] = data[i];
+        }
+
+        //now simply push the numbers into the original list
+        for (int i = 0; i < data.Count; i++) {
+            data[i] = result[i];
+        }
     }
 }
